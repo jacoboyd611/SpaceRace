b178560de7cc84d903526b6149f9040e129d7675
Jacob Boyd <jacoboyd611@outlook.com>
2021-03-23 14:19:03 -0400

    Finish

--- SpaceRace/Form1.Designer.cs
+++ SpaceRace/Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.p1ScoreLabel = new System.Windows.Forms.Label();
             this.p2ScoreLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.countTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // gameTimer
@@ -51,6 +54,7 @@
             this.p1ScoreLabel.Size = new System.Drawing.Size(35, 37);
             this.p1ScoreLabel.TabIndex = 0;
             this.p1ScoreLabel.Text = "0";
+            this.p1ScoreLabel.Visible = false;
             // 
             // p2ScoreLabel
             // 
@@ -63,6 +67,36 @@
             this.p2ScoreLabel.Size = new System.Drawing.Size(35, 37);
             this.p2ScoreLabel.TabIndex = 1;
             this.p2ScoreLabel.Text = "0";
+            this.p2ScoreLabel.Visible = false;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.Font = new System.Drawing.Font("Cooper BlkItHd BT", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.OrangeRed;
+            this.outputLabel.Location = new System.Drawing.Point(139, 87);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(450, 100);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "SPACE RACE";
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.instructionLabel.Font = new System.Drawing.Font("italic 08_55", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.instructionLabel.Location = new System.Drawing.Point(134, 172);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(455, 24);
+            this.instructionLabel.TabIndex = 3;
+            this.instructionLabel.Text = "Press space to start or escape to exit";
+            this.instructionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // countTimer
+            // 
+            this.countTimer.Interval = 20;
+            this.countTimer.Tick += new System.EventHandler(this.CountTimer_Tick);
             // 
             // Form1
             // 
@@ -70,8 +104,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(717, 491);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.p2ScoreLabel);
             this.Controls.Add(this.p1ScoreLabel);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
@@ -87,6 +124,9 @@
         private System.Windows.Forms.Timer gameTimer;
         private System.Windows.Forms.Label p1ScoreLabel;
         private System.Windows.Forms.Label p2ScoreLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Timer countTimer;
     }
 }
 
--- SpaceRace/Form1.cs
+++ SpaceRace/Form1.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿///Jacob Boyd
+///Mr T.
+///March 23
+///Recreation of classic arcade game Space Race
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +12,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Media;
 
 namespace SpaceRace
 {
@@ -16,24 +23,32 @@ namespace SpaceRace
         {
             InitializeComponent();
         }
+
         #region Variables
-        int width = 683;
+        bool running = false;
+
+        bool endscreen = false;
+
+        int p1Score = 0;
+        int p2Score = 0;
+
+        int width = 715;
         int height = 460;
 
         int player1X = 250;
         int player1Y = 450;
 
-
         int player2X = 450;
         int player2Y = 450;
 
-        int playerWidth = 10;
+        int playerWidth = 13;
         int playerHeight = 30;
         int playerSpeed = 7;
 
         int asteroidWidth = 5;
         int asteroidHeight = 5;
-        int asteroidSpeed = 5;
+
+        int timerLine = 37;
 
         bool wDown = false;
         bool sDown = false;
@@ -41,13 +56,24 @@ namespace SpaceRace
         bool upArrowDown = false;
         bool downArrowDown = false;
 
+        Pen drawPen = new Pen(Color.Blue, 3);
+
         SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush OrangeBrush = new SolidBrush(Color.OrangeRed);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
 
         List<int> asteroidY = new List<int>();
         List<int> asteroidX = new List<int>();
         List<int> direction = new List<int>();
 
         Random randGen = new Random();
+
+        SoundPlayer explosion = new SoundPlayer(Properties.Resources.explosion);
+        SoundPlayer airHorn = new SoundPlayer(Properties.Resources.airHorn);
+        SoundPlayer finishLine = new SoundPlayer(Properties.Resources.finishLine);
+
         #endregion
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -61,12 +87,23 @@ namespace SpaceRace
                     sDown = true;
                     break;
 
-
                 case Keys.Up:
                     upArrowDown = true;
                     break;
                 case Keys.Down:
                     downArrowDown = true;
+                    break;
+
+                case Keys.Space:                   
+                    running = true;
+                    outputLabel.Visible = false;
+                    instructionLabel.Visible = false;
+                    p1ScoreLabel.Visible = true;
+                    p2ScoreLabel.Visible = true;
+                    break;
+
+                case Keys.Escape:
+                    this.Close();
                     break;
             }
         }
@@ -78,14 +115,15 @@ namespace SpaceRace
                 case Keys.W:
                     wDown = false;
                     break;
+
                 case Keys.S:
                     sDown = false;
                     break;
 
-
                 case Keys.Up:
                     upArrowDown = false;
                     break;
+
                 case Keys.Down:
                     downArrowDown = false;
                     break;
@@ -94,94 +132,212 @@ namespace SpaceRace
 
         private void GameTimer_Tick(object sender, EventArgs e)
         {
-            //randomize starting positions and directions
-            if (asteroidY.Count == 0)
+            if (running)
             {
-                for (int i = 0; i < 25; i++)
+                //start countdown timer
+                countTimer.Enabled = true;
+
+                //randomize starting positions and directions
+                if (asteroidY.Count == 0)
                 {
-                    asteroidY.Add(randGen.Next(50, 400));
-                    asteroidX.Add(randGen.Next(0, width));
-                    int positiveNegative = randGen.Next(0, 2);
-                    if (positiveNegative == 1) { direction.Add(asteroidSpeed); }
-                    else { direction.Add(asteroidSpeed*-1); }
+                    airHorn.Play();
+                    for (int i = 0; i < 25; i++)
+                    {
+                        asteroidY.Add(randGen.Next(50, 400));
+                        asteroidX.Add(randGen.Next(0, width));
+                        int asteroidSpeed = randGen.Next(3, 10);
+                        int positiveNegative = randGen.Next(0, 2);
+                        if (positiveNegative == 1) { direction.Add(asteroidSpeed); }
+                        else { direction.Add(asteroidSpeed * -1); }
+                    }
                 }
-            }
 
-            //move ball
-            for (int i = 0; i < asteroidX.Count; i++)
-            {
-                asteroidX[i] += direction[i];
-                if (asteroidX[i] < 0) { direction[i] = direction[i] * -1; }
-                if (asteroidX[i] > width) { direction[i] = direction[i] * -1; }
-            }
+                //move asteroids
+                for (int i = 0; i < asteroidX.Count; i++)
+                {
+                    asteroidX[i] += direction[i];
+                    if (asteroidX[i] < 0) { direction[i] = direction[i] * -1; }
+                    if (asteroidX[i] > width) { direction[i] = direction[i] * -1; }
+                }
 
-            #region Player Movement
+                #region Player Movement
                 //move player1
                 if (wDown == true && player1Y > 0)
-            {
-                player1Y -= playerSpeed;
-            }
-
-            if (sDown == true && player1Y < height - playerHeight)
-            {
-                player1Y += playerSpeed;
-            }
-
-            //move player2
-            if (upArrowDown == true && player2Y > 0)
-            {
-                player2Y -= playerSpeed;
-            }
-
-            if (downArrowDown == true && player2Y < height - playerHeight)
-            {
-                player2Y += playerSpeed;
-            }
-            #endregion
-
-            #region Player Intersection
-            //P1
-            Rectangle player1 = new Rectangle(player1X, player1Y, playerWidth, playerHeight);
-            for (int i = 0; i < asteroidX.Count; i++)
-            {
-                Rectangle asteroid = new Rectangle(asteroidX[i], asteroidY[i], asteroidWidth, asteroidHeight);
-                if (player1.IntersectsWith(asteroid))
                 {
+                    player1Y -= playerSpeed;
+                }
+
+                if (sDown == true && player1Y < height - playerHeight)
+                {
+                    player1Y += playerSpeed;
+                }
+
+                //move player2
+                if (upArrowDown == true && player2Y > 0)
+                {
+                    player2Y -= playerSpeed;
+                }
+
+                if (downArrowDown == true && player2Y < height - playerHeight)
+                {
+                    player2Y += playerSpeed;
+                }
+                #endregion
+
+                #region Player Intersection
+                //P1
+                Rectangle player1 = new Rectangle(player1X, player1Y, playerWidth, playerHeight);
+                for (int i = 0; i < asteroidX.Count; i++)
+                {
+                    Rectangle asteroid = new Rectangle(asteroidX[i], asteroidY[i], asteroidWidth, asteroidHeight);
+                    if (player1.IntersectsWith(asteroid))
+                    {
+                        player1X = 250;
+                        player1Y = 450;
+                        explosion.Play();
+                    }
+                }
+
+                //P2
+                Rectangle player2 = new Rectangle(player2X, player2Y, playerWidth, playerHeight);
+                for (int i = 0; i < asteroidX.Count; i++)
+                {
+                    Rectangle asteroid = new Rectangle(asteroidX[i], asteroidY[i], asteroidWidth, asteroidHeight);
+                    if (player2.IntersectsWith(asteroid))
+                    {
+                        player2X = 450;
+                        player2Y = 450;
+                        explosion.Play();
+                    }
+                }
+
+                //end line
+                Rectangle endLine = new Rectangle(0, 0, width, 50);
+                if (player1.IntersectsWith(endLine))
+                {
+                    p1Score++;
                     player1X = 250;
                     player1Y = 450;
+                    finishLine.Play();
                 }
-            }
-
-            //P2
-            Rectangle player2 = new Rectangle(player2X, player2Y, playerWidth, playerHeight);
-            for (int i = 0; i < asteroidX.Count; i++)
-            {
-                Rectangle asteroid = new Rectangle(asteroidX[i], asteroidY[i], asteroidWidth, asteroidHeight);
-                if (player2.IntersectsWith(asteroid))
+                if (player2.IntersectsWith(endLine))
                 {
+                    p2Score++;
                     player2X = 450;
                     player2Y = 450;
+                    finishLine.Play();
                 }
+                #endregion
+
+                p1ScoreLabel.Text = $"{p1Score}";
+                p2ScoreLabel.Text = $"{p2Score}";                
             }
+            else if (running == false)
+            {
+                #region clear and output winner
+                player1X = 250;
+                player1Y = 450;
 
-            #endregion
+                player2X = 450;
+                player2Y = 450;
 
+                timerLine = 37;
+
+                
+
+                asteroidX.Clear();
+                asteroidY.Clear();
+                direction.Clear();
+
+                p1ScoreLabel.Visible = false;
+                p2ScoreLabel.Visible = false;
+                outputLabel.Visible = true;
+                instructionLabel.Visible = true;
+
+                if (endscreen)
+                {
+                    if (p1Score > p2Score) { outputLabel.Text = "PLAYER 1 WINS"; }
+                    else if (p1Score < p2Score) { outputLabel.Text = "PLAYER 2 WINS"; }
+                    else if (p1Score == p2Score) { outputLabel.Text = "DRAW"; }
+                    endscreen = false;
+                    p1Score = 0;
+                    p2Score = 0;
+                }              
+                #endregion 
+            }
             Refresh();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            //draw players
-            e.Graphics.FillRectangle(whiteBrush, player1X, player1Y, playerWidth, playerHeight);
-            e.Graphics.FillRectangle(whiteBrush, player2X, player2Y, playerWidth, playerHeight);
-
-            e.Graphics.FillRectangle(whiteBrush, 355, 50, 10, 800);
-
-            //draw asteroids
-            for (int i = 0; i < asteroidY.Count; i++)
+            if (running)
             {
-                e.Graphics.FillRectangle(whiteBrush, asteroidX[i], asteroidY[i], asteroidWidth, asteroidHeight);         
+                //end line
+                e.Graphics.FillRectangle(greenBrush, 0, 50, width, 5);
+
+                //draw players
+                #region P1
+                e.Graphics.DrawLine(drawPen, player1X + 5, player1Y, player1X + playerWidth -5, player1Y);
+                e.Graphics.DrawLine(drawPen, player1X + 5, player1Y, player1X , player1Y + 5);
+                e.Graphics.DrawLine(drawPen, player1X + playerWidth - 5, player1Y, player1X + playerWidth, player1Y + 5);
+
+                e.Graphics.DrawLine(drawPen, player1X , player1Y + 5, player1X, player1Y + 20);
+                e.Graphics.DrawLine(drawPen, player1X, player1Y + 20, player1X - 5, player1Y + 10);
+                e.Graphics.DrawLine(drawPen, player1X + 3, player1Y + playerHeight, player1X - 5, player1Y + 20);
+                e.Graphics.DrawLine(drawPen, player1X - 5, player1Y + 10, player1X - 5, player1Y + 20);
+
+                e.Graphics.DrawLine(drawPen, player1X + playerWidth, player1Y + 5, player1X + playerWidth, player1Y + 20);
+                e.Graphics.DrawLine(drawPen, player1X + playerWidth, player1Y + 20, player1X + playerWidth + 5, player1Y + 10);
+                e.Graphics.DrawLine(drawPen, player1X + playerWidth - 3, player1Y + playerHeight, player1X + playerWidth + 5, player1Y + 20);
+                e.Graphics.DrawLine(drawPen, player1X + playerWidth + 5, player1Y + 10, player1X + playerWidth + 5, player1Y + 20);
+
+                e.Graphics.DrawLine(drawPen, player1X + playerWidth - 5, player1Y + playerHeight, player1X + 5, player1Y + playerHeight);
+                #endregion
+                drawPen.Color = Color.Red;
+                #region P2
+                e.Graphics.DrawLine(drawPen, player2X + 5, player2Y, player2X + playerWidth - 5, player2Y);
+                e.Graphics.DrawLine(drawPen, player2X + 5, player2Y, player2X, player2Y + 5);
+                e.Graphics.DrawLine(drawPen, player2X + playerWidth - 5, player2Y, player2X + playerWidth, player2Y + 5);
+
+                e.Graphics.DrawLine(drawPen, player2X, player2Y + 5, player2X, player2Y + 20);
+                e.Graphics.DrawLine(drawPen, player2X, player2Y + 20, player2X - 5, player2Y + 10);
+                e.Graphics.DrawLine(drawPen, player2X + 3, player2Y + playerHeight, player2X - 5, player2Y + 20);
+                e.Graphics.DrawLine(drawPen, player2X - 5, player2Y + 10, player2X - 5, player2Y + 20);
+
+                e.Graphics.DrawLine(drawPen, player2X + playerWidth, player2Y + 5, player2X + playerWidth, player2Y + 20);
+                e.Graphics.DrawLine(drawPen, player2X + playerWidth, player2Y + 20, player2X + playerWidth + 5, player2Y + 10);
+                e.Graphics.DrawLine(drawPen, player2X + playerWidth - 3, player2Y + playerHeight, player2X + playerWidth + 5, player2Y + 20);
+                e.Graphics.DrawLine(drawPen, player2X + playerWidth + 5, player2Y + 10, player2X + playerWidth + 5, player2Y + 20);
+
+                e.Graphics.DrawLine(drawPen, player2X + playerWidth - 5, player2Y + playerHeight, player2X + 5, player2Y + playerHeight);
+                #endregion
+                drawPen.Color = Color.Blue;
+
+                //draw asteroids
+                for (int i = 0; i < asteroidY.Count; i++)
+                {
+                    int colourFlash = randGen.Next(0, 2);
+                    if (colourFlash == 1)
+                    { e.Graphics.FillRectangle(OrangeBrush, asteroidX[i], asteroidY[i], asteroidWidth, asteroidHeight); }
+                    else
+                    { e.Graphics.FillRectangle(whiteBrush, asteroidX[i], asteroidY[i], asteroidWidth, asteroidHeight); }
+                }
+                e.Graphics.FillRectangle(whiteBrush, 355, timerLine, 10, height);
+            }
+        }
+
+        private void CountTimer_Tick(object sender, EventArgs e)
+        {
+            //makes the line move down and detects when it is off the screen
+            timerLine++;
+            if (timerLine > 500)
+            {
+                airHorn.Play();
+                countTimer.Enabled = false;
+                running = false;
+                endscreen = true;
             }
         }
     }
 }
+
